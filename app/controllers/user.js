5219790e6fc76ce3d0f71d7f78135577706dbe6e
Tim <timrchen@foxmail.com>
2017-08-19 23:50:00 +0800

    update signup api

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,21 +1,5 @@
 const UserModel = require('../models/user');
 
-// showSignup
-exports.showSignup = function(req, res) {
-	res.render('signup', {
-		title: '注册页面',
-		poster: 'background-image: url(/images/book.jpg)',
-	});
-};
-
-// showSignin
-exports.showSignin = function(req, res) {
-	res.render('signin', {
-		title: '登陆页面',
-		poster: 'background-image: url(/images/book.jpg)',
-	});
-};
-
 /* signUp */
 exports.signup = function(req, res) {
 	let _user = req.body;
@@ -25,53 +9,52 @@ exports.signup = function(req, res) {
 			console.log(err);
 		}
 		// 处理用户名重复
-		if (user) {
-			return res.redirect('/signin');
-		} else {
-			user = new UserModel(_user);
-			user.save(function(err, user) {
-				if (err) {
-					console.log(err);
-				}
-				res.redirect('/');
-			});
-		}
+		// if (user) {
+		// 	return res.redirect('/signin');
+		// } else {
+		user = new UserModel(_user);
+		user.save(function(err, user) {
+			if (err) {
+				console.log(err);
+			}
+		});
+		// }
 	});
 };
 
 
 /* Login */
-exports.signin = function(req, res) {
-	let _user = req.body;
-	let name = _user.name;
-	let password = _user.password;
+// exports.signin = function(req, res) {
+// 	let _user = req.body;
+// 	let name = _user.name;
+// 	let password = _user.password;
 
-	UserModel.findOne({name: name}, function(err, user) {
-		if (err) {
-			console.log(err);
-		}
-		// user不存在，返回注册页
-		if (!user) {
-			// return res.redirect('/signup');
-			return res.redirect('/');
-		}
-		// 密码校对
-		user.comparePassword(password, function(err, result) {
-			if (err) {
-				console.log(err);
-			}
+// 	UserModel.findOne({name: name}, function(err, user) {
+// 		if (err) {
+// 			console.log(err);
+// 		}
+// 		// user不存在，返回注册页
+// 		if (!user) {
+// 			// return res.redirect('/signup');
+// 			return res.redirect('/');
+// 		}
+// 		// 密码校对
+// 		user.comparePassword(password, function(err, result) {
+// 			if (err) {
+// 				console.log(err);
+// 			}
 
-			if (result) {
-				req.session.user = user;
-				console.log('Password is matched');
-				return res.redirect('/');
-			} else {
-				console.log('Password is not matched');
-				return res.redirect('/signin');	// 密码不正确，则重定向至登录页
-			}
-		});
-	});
-};
+// 			if (result) {
+// 				req.session.user = user;
+// 				console.log('Password is matched');
+// 				return res.redirect('/');
+// 			} else {
+// 				console.log('Password is not matched');
+// 				return res.redirect('/signin');	// 密码不正确，则重定向至登录页
+// 			}
+// 		});
+// 	});
+// };
 
 
 /* logout */
@@ -82,59 +65,59 @@ exports.logout = function(req, res) {
 
 
 /* userList page */
-exports.list = function(req, res) {
-	UserModel.fetch(function(err, users) {
-		if (err) {
-			console.log(err);
-		}
-		res.render('userList', {
-			poster: 'background-image: url(/images/book.jpg)',
-			title: '用户列表页',
-			users: users,
-		});
-	});
-};
+// exports.list = function(req, res) {
+// 	UserModel.fetch(function(err, users) {
+// 		if (err) {
+// 			console.log(err);
+// 		}
+// 		res.render('userList', {
+// 			poster: 'background-image: url(/images/book.jpg)',
+// 			title: '用户列表页',
+// 			users: users,
+// 		});
+// 	});
+// };
 
 // list delete user
-exports.delete = function(req, res, next) {
-	const id = req.query.id;
-	if (id) {
-		UserModel.findById({_id: id}, function(err, user) {
-			if (err) {
-				console.log(err);
-			}
-			// 删除文章内容
-			user.remove(function(err) {
-				if (err) {
-					console.log(err);
-					res.json({success: 0});
-				} else {
-					res.json({success: 1});
-				}
-			});
-		});
-	}
-};
+// exports.delete = function(req, res, next) {
+// 	const id = req.query.id;
+// 	if (id) {
+// 		UserModel.findById({_id: id}, function(err, user) {
+// 			if (err) {
+// 				console.log(err);
+// 			}
+// 			// 删除文章内容
+// 			user.remove(function(err) {
+// 				if (err) {
+// 					console.log(err);
+// 					res.json({success: 0});
+// 				} else {
+// 					res.json({success: 1});
+// 				}
+// 			});
+// 		});
+// 	}
+// };
 
 
 /* middleware for user */
-exports.signinRequired = function(req, res, next) {
-	let user = req.session.user;
-	console.log(user);
+// exports.signinRequired = function(req, res, next) {
+// 	let user = req.session.user;
+// 	console.log(user);
 
-	if (!user) {
-		return res.redirect('/signin');
-	}
+// 	if (!user) {
+// 		return res.redirect('/signin');
+// 	}
 
-	next();
-};
+// 	next();
+// };
 
 /* middleware for admin */
-exports.adminRequired = function(req, res, next) {
-	let user = req.session.user;
-	if (user.role <= 10) {
-		return res.redirect('/');
-	}
+// exports.adminRequired = function(req, res, next) {
+// 	let user = req.session.user;
+// 	if (user.role <= 10) {
+// 		return res.redirect('/');
+// 	}
 
-	next();
-};
+// 	next();
+// };
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 const mongoose = require('mongoose');
 const session = require('express-session');
 const mongoStore = require('connect-mongo')(session);
-const dbUrl = 'mongodb://localhost/essay';
-const Index = require('../app/controllers/index');
+const dbUrl = 'mongodb://localhost/timrchenDB';
+// const Index = require('../app/controllers/index');
 const User = require('../app/controllers/user');
-const Essay = require('../app/controllers/essay');
+// const Essay = require('../app/controllers/essay');
 
 mongoose.Promise = global.Promise;  // 赋值一个全局Promise
 mongoose.connect(dbUrl, {useMongoClient: true});
@@ -27,30 +27,41 @@ router.use(function(req, res, next) {
 	next();
 });
 
+// CORS
+router.all('*', function(req, res, next) {  
+    //设置允许 http://localhost:8080 这个域响应  
+    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:8080");  
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");  
+    res.header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Authorization, Accept,X-Requested-With");  
+    next();
+  });
+
 
 /* Index */
-router.get('/', Index.index);
-router.get('/next', Index.next);
-router.get('/previous', Index.next);
+// router.get('/', Index.index);
+// router.get('/next', Index.next);
+// router.get('/previous', Index.next);
 
 
 /* User */
-// router.post('/user/signup', User.signup);
-router.post('/user/signin', User.signin);
-router.get('/signin', User.showSignin);
+router.post('/signup', User.signup);
+
+
+// router.post('/user/signin', User.signin);
+// router.get('/signin', User.showSignin);
 // router.get('/signup', User.showSignup);
-router.get('/logout', User.logout);
-router.get('/admin/user/list', User.signinRequired, User.adminRequired, User.list);
-router.delete('/admin/user/list', User.signinRequired, User.adminRequired, User.delete);
+// router.get('/logout', User.logout);
+// router.get('/admin/user/list', User.signinRequired, User.adminRequired, User.list);
+// router.delete('/admin/user/list', User.signinRequired, User.adminRequired, User.delete);
 
 
 /* Essay */
-router.get('/essay/:id', Essay.detail);
-router.get('/admin/essay/new', User.signinRequired, User.adminRequired, Essay.new);
-router.post('/admin/essay', User.signinRequired, User.adminRequired, Essay.save);
-router.get('/admin/essay/update/:id', User.signinRequired, User.adminRequired, Essay.update);
-router.get('/admin/essay/list', User.signinRequired, User.adminRequired, Essay.list);
-router.delete('/admin/essay/list', User.signinRequired, User.adminRequired, Essay.delete);
+// router.get('/essay/:id', Essay.detail);
+// router.get('/admin/essay/new', User.signinRequired, User.adminRequired, Essay.new);
+// router.post('/admin/essay', User.signinRequired, User.adminRequired, Essay.save);
+// router.get('/admin/essay/update/:id', User.signinRequired, User.adminRequired, Essay.update);
+// router.get('/admin/essay/list', User.signinRequired, User.adminRequired, Essay.list);
+// router.delete('/admin/essay/list', User.signinRequired, User.adminRequired, Essay.delete);
 
 
 
